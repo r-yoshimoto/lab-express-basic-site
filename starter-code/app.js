5fb825fe128df115a4922147726d14c9dfb0c2c1
Rodrigo Yoshimoto <rodrigo.yoshimoto@gmail.com>
2019-01-19 17:16:33 -0200

    My favorite artist of all time cleanup

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,6 +8,14 @@ app.get('/', (request, response, next) => {
   response.sendFile(__dirname + '/index.html');
 });
 
+app.get('/about.html', (request, response, next) => {
+  response.sendFile(__dirname + '/about.html');
+});
+
+app.get('/photogallery.html', (request, response, next) => {
+  response.sendFile(__dirname + '/photogallery.html');
+});
+
 app.listen(3000, () => {
   console.log('Go see my favorite artist on localhost:3000!');
 });
